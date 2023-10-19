d7d6908d826f1a8ecfbf2ca2ffe81b860aadaed6
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2023-10-19 21:55:54 +0530

    Update ShuffleArray.java

--- Java/ShuffleArray.java
+++ Java/ShuffleArray.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Main {
+public class ShuffleArray{
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7};
         shuffleArray(array);
